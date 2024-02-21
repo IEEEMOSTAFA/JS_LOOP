ec20bd0ec2483c8520c43a1c7b1149e6769a5fb9
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-02-21 16:09:38 +0600

    even part

---
+++ even.js
@@ -0,0 +1,33 @@
+let num = 1;
+while(num <= 10){
+    
+    
+    if(num % 2 === 0){
+        console.log('even number',num);
+    } 
+    num++;
+      
+}
+
+
+
+// let i 
+// let a = 100
+// console.log("All even numbers from 1 to 100")
+// for (i=1;i<=a;i++)
+// {
+//   if(i%2==0){
+//     console.log(i)
+//   }
+// }
+// ----------------------
+
+
+// let i= 1;
+// while(i <= 10)
+// {
+//     if(i%2==0){
+//         console.log(i);
+//     }
+//     i++;
+// }
